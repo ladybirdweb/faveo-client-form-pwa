9938cc42e19ea89bfd2efc3089996a70182cfbf9
Manish Verma <mverma16@outlook.com>
2021-08-13 16:29:00 +0530

    PWA configuration added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 const webpack = require('webpack');
-
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -41,12 +41,41 @@ mix.webpackConfig({
         ],
     },
 
-   plugins: [
-   	 	new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: "common",
-        filename: "public/js/common.js",
-        minChunks: 2
-      })
-   ],
+    plugins: [
+   	    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "common",
+            filename: "public/js/common.js",
+            minChunks: 2
+        }),
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'pwa',
+            filename: 'public/service-worker.js',
+            staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+            minify: true,
+            stripPrefix: 'public/',
+            handleFetch: true,
+            dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+               //and have full support for offline first (example below)
+                '/': ['public/index.html'],
+                // '/posts': ['resources/views/posts.blade.php']
+            },
+            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+            navigateFallback: '/',
+            // runtimeCaching: [
+            //     {
+            //         urlPattern: /^https:\/\/code\.jquery\.com\//,
+            //         handler: 'cacheFirst'
+            //     },
+            //     {
+            //         urlPattern: /^https:\/\/maxcdm\.bootstrapcdn\.com\//,
+            //         handler: 'cacheFirst'
+            //     },
+            //     {
+            //         urlPattern: /^https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/(\w+)\.jpg/,
+            //         handler: 'cacheFirst'
+            //     }
+            // ]
+        })
+    ],
 })
