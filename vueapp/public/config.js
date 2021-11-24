305cec461b586be43e9b801b53fe45d5e72e0c9a
Manish Verma <mverma16@outlook.com>
2021-11-24 11:20:28 +0530

    default printing added

--- vueapp/public/config.js
+++ vueapp/public/config.js
@@ -2,5 +2,6 @@ const config = {
 	"apiBaseURL" : location.host, // Put URL of your Faveo system here
 	"theme" : "navbar-light bg-light",
 	"color" : "#40A6D7",
-	"defaultRequester": 0 //Set ID of user if you want to use particular user for ticket requester else 0 to allow users to create new users
+	"defaultRequester": 0, //Set ID of user if you want to use particular user for ticket requester else 0 to allow users to create new users
+	"default_print":true //Tell to print the ticket number automatically or not
 }
